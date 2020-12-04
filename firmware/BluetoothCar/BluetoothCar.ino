ae9f046bd985251346bb23287cc62490f59287a6
vitaliy172 <68148475+vitaliy172@users.noreply.github.com>
2020-12-04 14:12:28 +0200

    Update BluetoothCar.ino

--- firmware/BluetoothCar/BluetoothCar.ino
+++ firmware/BluetoothCar/BluetoothCar.ino
@@ -18,10 +18,10 @@
 #define LEFT_MOTOR_MODE       HIGH    // смени HIGH на LOW если мотор включает тормоз
 
 /*=============== ПИНЫ ===============*/
-#define LEFT_MOTOR_D 2
-#define LEFT_MOTOR_PWM 3              // обязательно должен быть ШИМ пином!!!
 #define RIGHT_MOTOR_D 10
 #define RIGHT_MOTOR_PWM 11            // обязательно должен быть ШИМ пином!!!
+#define LEFT_MOTOR_D 2
+#define LEFT_MOTOR_PWM 3              // обязательно должен быть ШИМ пином!!!
 #define BT_TX 13
 #define BT_RX 12
 
@@ -43,19 +43,19 @@ boolean doneParsing, startParsing;
 int dataX, dataY;
 String string_convert;
 
-void setup(){
-  #if (LEFT_MOTOR_PWM == 3 || LEFT_MOTOR_PWM == 11 || RIGHT_MOTOR_PWM == 3 || RIGHT_MOTOR_PWM == 11)
+void setup() {
+#if (LEFT_MOTOR_PWM == 3 || LEFT_MOTOR_PWM == 11 || RIGHT_MOTOR_PWM == 3 || RIGHT_MOTOR_PWM == 11)
   //D3 и D11 62.5 kHz PWM (благоприятно для електродвигателей)
   TCCR2B = 0b00000001;
   TCCR2A = 0b00000011;
-  #elif (LEFT_MOTOR_PWM == 9 || LEFT_MOTOR_PWM == 10 || RIGHT_MOTOR_PWM == 9 || RIGHT_MOTOR_PWM == 10)
+#elif (LEFT_MOTOR_PWM == 9 || LEFT_MOTOR_PWM == 10 || RIGHT_MOTOR_PWM == 9 || RIGHT_MOTOR_PWM == 10)
   //D9 и D10 62.5 kHz PWM (благоприятно для електродвигателей)
   TCCR1A = 0b00000001;
   TCCR1B = 0b00001001;
-  #endif
-  
+#endif
+
   BTserial.begin(9600);
-  
+
   motorR.setMode(AUTO);
   motorL.setMode(AUTO);
   motorR.setMinDuty(minDuty);
@@ -64,11 +64,11 @@ void setup(){
   motorL.setDirection(LEFT_MOTOR_DIRECTION);
 }
 
-void loop(){
-  parsing();        // функция парсинга
-  if (doneParsing){ // если получены данные
+void loop() {
+  parsing();         // функция парсинга
+  if (doneParsing) { // если получены данные
     doneParsing = false;
-    
+
     int joystickX = map((dataX), -JOY_MAX, JOY_MAX, -MOTOR_MAX / 2, MOTOR_MAX / 2); // сигнал по Х
     int joystickY = map((dataY), -JOY_MAX, JOY_MAX, -MOTOR_MAX, MOTOR_MAX);         // сигнал по Y
 
@@ -77,30 +77,30 @@ void loop(){
 
     dutyR = constrain(dutyR, -MOTOR_MAX, MOTOR_MAX); // ограничиваем значение для dutyR, на случай если dutyR > 255
     dutyL = constrain(dutyL, -MOTOR_MAX, MOTOR_MAX); // ограничиваем значение для dutyL, на случай если dutyL > 255
-    
+
     motorR.smoothTick(dutyR); // плавно крутим правый мотор
     motorL.smoothTick(dutyL); // плавно крутим левый мотор
   }
 }
 
-void parsing(){
-  if (BTserial.available() > 0){        // если в буфере есть данные
+void parsing() {
+  if (BTserial.available() > 0) {       // если в буфере есть данные
     char incomingChar = BTserial.read();// читаем из буфера
-    if (startParsing){                  // начать принятие пакетов
-      if (incomingChar == ' '){         // принят пакет dataX
-        dataX = string_convert.toInt(); // ковертируем принятый пакет в переменную
+    if (startParsing) {                 // начать принятие пакетов
+      if (incomingChar == ' ') {        // принят пакет dataX
+        dataX = string_convert.toInt(); // ковертируем принятый пакет в число
         string_convert = "";            // очищаем переменную пакета
       }
-      else if (incomingChar == ';'){    // принят пакет dataY
-        dataY = string_convert.toInt(); // ковертируем принятый пакет в переменную
+      else if (incomingChar == ';') {   // принят пакет dataY
+        dataY = string_convert.toInt(); // ковертируем принятый пакет в число
         string_convert = "";            // очищаем переменную пакета
         startParsing = false;           // закончить принятие пакетов
         doneParsing = true;             // парсинг окончен, можно переходить к движению
-      }else{
+      } else {
         string_convert += incomingChar; // записываем  принятые данные в переменную
       }
     }
-    if (incomingChar == '$'){           // начало парсинга
+    if (incomingChar == '$') {          // начало парсинга
       startParsing = true;              // начать принятие пакетов
     }
   }
